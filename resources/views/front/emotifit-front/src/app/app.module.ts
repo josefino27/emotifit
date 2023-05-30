4cfc5b9277438b0ca3caca1d16fae1cee4d8ed6b
josefino27 <jodanu19@gmail.com>
2023-05-30 08:35:08 -0500

    rem reserva

--- resources/views/front/emotifit-front/src/app/app.module.ts
+++ resources/views/front/emotifit-front/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReservaComponent } from './reserva/reserva.component';
 import { ClaseComponent } from './components/clases/clase/clase.component';
 import { ClasesComponent } from './components/clases/clases.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReservaComponent,
     ClaseComponent,
     ClasesComponent
   ],
