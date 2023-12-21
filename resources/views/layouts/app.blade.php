2b4e0f6cf8eac6b354b9aa3fe007118d0a25fece
josefino27 <jodanu19@gmail.com>
2023-12-21 12:58:43 -0500

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'Emotifit') }}</title>
 
     <!-- Scripts -->
