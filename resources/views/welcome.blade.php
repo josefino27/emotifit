82770a4e0905672eacbb2897ca330883d0ade216
josefino27 <jodanu19@gmail.com>
2023-12-21 13:09:25 -0500

    style welccome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,14 +15,12 @@
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
         <!-- bootstrap 5.0 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- Core theme CSS (includes Bootstrap) Tema CSS de la plantilla startbootstrap-->
         <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
-         <!-- Our Custom CSS -->
-         <link rel="stylesheet" href="{{asset('css/sidebar.css')}}">
+
 
         <!-- Font Awesome JS -->
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
