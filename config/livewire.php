0adb5b7abe4ce2323a20eebe50b979c05823cb4e
josefino27 <jodanu19@gmail.com>
2023-12-21 01:45:03 -0500

    base_path bootstrap livewire prod

--- config/livewire.php
+++ config/livewire.php
@@ -122,7 +122,7 @@ return [
     |
     */
 
-    'manifest_path' => null,
+    'manifest_path' =>  base_path('bootstrap/cache/livewire-components.php'),
 
     /*
     |--------------------------------------------------------------------------
